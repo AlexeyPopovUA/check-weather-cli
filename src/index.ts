3a361623a5a491494bbbbe0d108a2c1d21fbbe0c
Oleksii Popov <alexejpopovua@gmail.com>
2022-10-02 17:51:03 +0200

    refactor: restructure the cli options cfg

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@
 import os from "os";
 import path from "path";
 import inquirer from "inquirer";
-import yargs from "yargs";
+import yargs, {Arguments} from "yargs";
 import {hideBin} from "yargs/helpers";
 
 import cfg from "./configuration/configuration";
-import options from "./options";
+import options, {CLIParams} from "./options";
 import TaskConfiguration, {TaskConfigurationProps} from "./task-configuration";
-import {TemperatureUnits} from "./types/units";
 import OutputWeatherToStdoutCommand from "./commands/output-weather-to-stdout-command";
 import WeatherCommand from "./commands/weather-command";
 import ImportTasksFromFileCommand from "./commands/import-tasks-from-file-command";
@@ -17,36 +16,46 @@ import GeolocationCommand from "./commands/geolocation-command";
 import OutputTasksToFileCommand from "./commands/output-tasks-to-file-command";
 
 (async () => {
+    const args = hideBin(process.argv);
     /**
      * No flags = display the prompt
      * With flags = no prompt
      */
-    const displayPrompt = hideBin(process.argv).length === 0;
+    const displayPrompt = args.length === 0;
 
     if (displayPrompt) {
-        const answers = await inquirer.prompt(Object.values(options));
+        const inquirerQuestions = Object.values(options)
+            // take only questions with inquirer configs
+            .filter(v => v.inquirer)
+            .map(v => v.inquirer);
+        const answers = await inquirer.prompt(inquirerQuestions);
 
         Object.entries(answers).forEach(([key, value]) => {
-            value && process.argv.push(`--${key}`, value);
+            value && args.push(`--${key}`, value);
         });
     }
 
-    const argv = await yargs(hideBin(process.argv)).options(options).parse();
+    const yargsOptions = Object.entries(options).reduce((acc, [k, v]) => {
+        Object.assign(acc, {[k]: v.yargs});
+        return acc;
+    }, {});
+
+    const argv = await yargs(args).options(yargsOptions).parse() as Arguments & Partial<CLIParams>;
 
     const cfgs: TaskConfigurationProps[] = [];
 
     // check the "latest" flag then get cfgs
-    if (argv["latest"]) {
+    if (argv.latest) {
         const cfgsFromFile = await new ImportTasksFromFileCommand({filePath: path.resolve(os.homedir(), cfg.SAVING_DIR, cfg.LATEST_FILE)}).execute();
         Array.prototype.push.apply(cfgs, cfgsFromFile);
-    } else if (argv["import"]) {
-        const cfgsFromFile = await new ImportTasksFromFileCommand({filePath: argv["import"] as string}).execute();
+    } else if (argv.import) {
+        const cfgsFromFile = await new ImportTasksFromFileCommand({filePath: argv.import}).execute();
         Array.prototype.push.apply(cfgs, cfgsFromFile);
     } else {
         cfgs.push({
-            temperatureUnit: argv["temperature"] as TemperatureUnits,
-            location: argv["zip"] as string,
-            useGeolocation: argv["use-geolocation"] as boolean
+            temperatureUnit: argv.temperature,
+            location: argv.zip,
+            useGeolocation: !!argv.geolocation
         });
     }
 
@@ -57,14 +66,16 @@ import OutputTasksToFileCommand from "./commands/output-tasks-to-file-command";
     }));
 
     // do not save for importing and querying latest
-    if (!argv["latest"] && !argv["import"]) {
+    if (!argv.latest && !argv.import) {
         await new OutputTasksToFileCommand({taskConfigurations}).execute();
     }
 
     // if no location - detect it and then request the weather stats
     const weatherRecords = await Promise.all(taskConfigurations.map(async taskConfiguration => {
-        if (!taskConfiguration.location && taskConfiguration.useGeolocation !== false) {
+        if (!taskConfiguration.location && taskConfiguration.useGeolocation) {
             taskConfiguration.location = await new GeolocationCommand({taskConfiguration}).execute();
+        } else if (!taskConfiguration.location && taskConfiguration.useGeolocation) {
+            throw new Error("Cannot detect location when it is disabled");
         }
         return new WeatherCommand({taskConfiguration}).execute();
     }))
--- src/options.ts
+++ src/options.ts
@@ -1,48 +1,88 @@
 import {Options} from "yargs";
 import {Question} from "inquirer";
 
-const options: {[k: string]: Options & Question} = {
-    // TODO SPLIT QUESTIONS AND OPTIONS
+import {TemperatureUnits} from "./types/units";
+
+export type CliOption = {
+    yargs: Options;
+    inquirer?: Question;
+};
+
+const options: {[k: string]: CliOption} = {
     import: {
-        // inquirer
-        message: 'Import options from file',
-        name: 'import',
-        // yargs
-        describe: 'Import options from file',
-        alias: "i",
-        default: ""
+        yargs: {
+            describe: 'Import options from file',
+            alias: "i",
+            default: ""
+        }
     },
     latest: {
-        // inquirer
-        message: 'Repeat the latest query',
-        name: 'latest',
-        // yargs
-        describe: 'Repeat the latest query',
-        alias: "l",
-        default: false,
-        type: "boolean"
+        yargs: {
+            describe: 'Repeat the latest query',
+            alias: "l",
+            default: false,
+            type: "boolean"
+        }
     },
     zip: {
-        // inquirer
-        message: 'What\'s the zipcode?',
-        name: 'zip',
-        // yargs
-        //demandOption: true,
-        describe: 'Query city by a zip code or a city name',
-        alias: "z",
-        // shared
-        type: 'string',
+        inquirer: {
+            message: 'What\'s the zipcode? (Example: 75015,fr)',
+            name: 'zip',
+            type: 'input'
+        },
+        yargs: {
+            describe: 'Query city by a zip code or a city name',
+            alias: "z",
+            type: 'string',
+            conflicts: "city"
+        }
+    },
+    city: {
+        inquirer: {
+            message: 'What\'s the city name?',
+            name: 'city',
+            type: 'string'
+        },
+        yargs: {
+            describe: 'Query by a city name',
+            alias: "c",
+            conflicts: "zip",
+            type: 'string'
+        }
     },
     temperature: {
-        // inquirer
-        message: 'Select unit of temperature in C or F',
-        name: 'temperature',
-        describe: 'Return temperatures in C or F',
-        // yargs
-        choices: ["C", "F"],
-        default: "C",
-        alias: "t"
+        inquirer: {
+            message: 'Select unit of temperature in C or F',
+            name: 'temperature',
+            default: "C",
+            type: "list",
+            // @ts-ignore
+            choices: ["C", "F"]
+        },
+        yargs: {
+            describe: 'Return temperatures in C or F',
+            choices: ["C", "F"],
+            default: "C",
+            alias: "t"
+        }
+    },
+    geolocation: {
+        yargs: {
+            describe: 'Use geolocation',
+            alias: "g",
+            default: true,
+            type: "boolean"
+        }
     }
 };
+
+export type CLIParams = {
+    import: string;
+    latest: string;
+    zip: string;
+    city: string;
+    temperature: TemperatureUnits;
+    geolocation: string;
+}
 
 export default options;
