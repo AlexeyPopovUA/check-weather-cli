0526910df8dc1b370b8d7b8dc21e62271a8b29fb
Oleksii Popov <alexejpopovua@gmail.com>
2022-10-02 15:05:17 +0200

    feat: ability to fetch location by ip

--- src/commands/geolocation-command.ts
+++ src/commands/geolocation-command.ts
@@ -1,10 +1,23 @@
-import AbstractTaskExecutor from "./abstract-task-executor";
+import got from "got";
+import template from "lodash/template";
 
+import AbstractTaskExecutor from "./abstract-task-executor";
+import cfg from "../configuration/configuration";
+
+type GeolocationResponse = {
+    zip: string;
+    country_code: string;
+};
 
 export default class GeolocationCommand extends AbstractTaskExecutor {
-    public static serviceURL: string = "";
+    // yes, http, not https. It is a pricing policy of https://ipapi.com
+    public static serviceURL: string = "http://api.ipapi.com/api/check?access_key=${apiKey}&fields=zip,country_code";
 
-    async execute() {
-        return "75015,fr";
+    async execute(): Promise<string> {
+        const response = await got.get(template(GeolocationCommand.serviceURL)({
+            apiKey: cfg.GEOLOCATION_API_KEY
+        })).json<GeolocationResponse>();
+
+        return `${response.zip},${response.country_code}`;
     }
 }
--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -6,6 +6,6 @@ dotenv.config({path: path.resolve("env/.env")});
 export default {
     LATEST_FILE: "latest.json",
     SAVING_DIR: ".check-weather-cli/",
-    GEOCODING_API_KEY: process.env.GEOCODING_API_KEY,
+    GEOLOCATION_API_KEY: process.env.GEOCODING_API_KEY,
     OPEN_WEATHER_API_KEY: process.env.OPEN_WEATHER_API_KEY
 };
